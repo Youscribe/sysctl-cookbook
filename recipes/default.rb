d82d3a0fc84505b8134d4447dc70cb7c001703e2
Guilhem Lettron <guilhem@lettron.fr>
2012-04-18 17:09:16 +0200

    !!!! change compatibility with previous version attributes are using hash now. see Readme.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,28 +5,18 @@
 # Copyright 2011, Fewbytes Technologies LTD
 #
 
-def compile_attr(prefix, v)
-  case v
-  when Array
-    return "#{prefix}=#{v.join(" ")}"
-  when String, Fixnum, Float, Symbol
-    "#{prefix}=#{v}"
-  when Hash, Chef::Node::Attribute
-    prefix += "." unless prefix.empty?
-    return v.map {|key, value| compile_attr("#{prefix}#{key}", value)}.flatten
-  else
-    raise Chef::Exceptions::UnsupportedAction, "Sysctl cookbook can't handle values of type: #{v.class}"
-  end
-end
-
 if node.attribute?(:sysctl)
-
-  attr_txt = compile_attr("", node[:sysctl]).join("\n") + "\n"
-
-  file "/etc/sysctl.d/50-chef-attributes.conf" do
-    content attr_txt
-    mode "0644"
-    notifies :start, "service[procps]"
+  node[:sysctl].each do |name, instructions|
+    template "/etc/sysctl.d/50-chef-attributes-#{name.gsub(" ", "_")}.conf" do
+      source "sysctl.conf.erb"
+      mode "0644"
+      owner "root"
+      group "root"
+      variables (
+        :instructions => instructions
+      )
+      notifies :start, "service[procps]"
+    end
   end
 end
 

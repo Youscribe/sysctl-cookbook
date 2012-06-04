93276018065a5d845e55f7b53e3c7ba227b0a0b8
Guilhem Lettron <guilhem@lettron.fr>
2012-06-04 12:04:14 +0200

    update files header update version

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-maintainer       "Fewbytes Technologies"
-maintainer_email "avishai@fewbytes.com"
+maintainer       "Guilhem Lettron"
+maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Configures sysctl parameters"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.0"
+version          "0.1.1"
 supports         "ubuntu"
 supports         "debian"

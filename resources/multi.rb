08eabdd8745f8d43ba1533e30d328a5d8c3ebb05
Evan Callicoat <diopter@gmail.com>
2012-06-20 15:45:34 -0500

    Add 'priority' attribute, default 40

--- resources/multi.rb
+++ resources/multi.rb
@@ -28,3 +28,4 @@ actions :save, :set, :remove
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :path, :kind_of => String, :default => nil
 attribute :instructions, :kind_of => Hash, :default => nil, :required => true
+attribute :priority, :kind_of => String, :default => "40"

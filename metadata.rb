3e047a839429ca1385c5b7a641c979eaa0ff43e0
Darren Birkett <darren.birkett@gmail.com>
2012-09-07 15:09:26 +0100

    fix readme location

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Guilhem Lettron"
 maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Configures sysctl parameters"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.5"
 %w{ debian ubuntu centos redhat }.each do |os|
       supports os

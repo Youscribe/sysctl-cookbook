6f1e9439c08fce467788e65421d3f4ad6cc3a345
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-10-03 10:57:25 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Configures sysctl parameters"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.5"
+version          "0.1.6"
 %w{ debian ubuntu centos redhat }.each do |os|
       supports os
 end
